807c69edfa891e242ac50b7a0b7c050b6d79c1d6
jhordy <jhordymundung10@gmail.com>
2021-10-31 10:24:42 +0800

    selesai di bagian password dengan login di menit 03:21:54

--- utils/data.js
+++ utils/data.js
@@ -1,4 +1,19 @@
+import bcrypt from 'bcryptjs';
+
 const data = {
+  users: [
+    {
+      name:'Jhon',
+      email: 'admin@example.com',
+      password: bcrypt.hashSync('123456'),
+      isAdmin: true,
+    },
+    {
+      name:'Jane',
+      email: 'user@example.com',
+      password: bcrypt.hashSync('123456'),
+      isAdmin: false,
+    },],
   products: [
     {
       name: 'Contoh Bunga Papan 1',
